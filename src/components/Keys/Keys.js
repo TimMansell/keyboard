a63c5b0463be77444969a23a88a4be02fc736bfc
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-05-01 17:45:43 +1000

    Play sound on key click

--- src/components/Keys/Keys.js
+++ src/components/Keys/Keys.js
@@ -3,10 +3,21 @@ import PropTypes from 'prop-types';
 import './keys.scss';
 
 function Keys({ type }) {
+  const handleClick = (key) => {
+    console.log('click', key);
+
+    const audio = new Audio(`sounds/${key}.ogg`);
+    audio.play();
+  };
+
   return (
     <div className="key">
       {type.map((key, index) => (
-        <div className="key__btn" key={index}></div>
+        <div
+          className="key__btn"
+          key={index}
+          onClick={() => handleClick(key)}
+        ></div>
       ))}
     </div>
   );
