a892b6878ac81379e57dad6160c21c9eb1841adb
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-05-23 20:32:30 +1000

    remove 88 and 76 key layout variations

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import keys25 from './config/25keys.json';
 import keys37 from './config/37keys.json';
 import keys49 from './config/49keys.json';
 import keys61 from './config/61keys.json';
-import keys76 from './config/76keys.json';
-import keys88 from './config/88keys.json';
 
 function App() {
   const keyLayout = {
@@ -14,8 +12,6 @@ function App() {
     keys37,
     keys49,
     keys61,
-    keys76,
-    keys88,
   };
 
   const [chosenLayout, setChosenLayout] = useState('keys37');
@@ -26,6 +22,7 @@ function App() {
 
   return (
     <div className="app">
+      <Piano keys={keyLayout[chosenLayout]} />
       <div className="choose-layout">
         Choose key layout:&nbsp;
         <select value={chosenLayout} onChange={(event) => setKeyLayout(event)}>
@@ -33,11 +30,8 @@ function App() {
           <option value="keys37">37 keys</option>
           <option value="keys49">49 keys</option>
           <option value="keys61">61 keys</option>
-          <option value="keys76">76 keys</option>
-          <option value="keys88">88 keys</option>
         </select>
       </div>
-      <Piano keys={keyLayout[chosenLayout]} />
     </div>
   );
 }
