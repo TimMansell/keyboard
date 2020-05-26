dc45aa30efbed69794fc7fff891ed87642bca7e5
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-05-26 11:27:07 +1000

    Move Tone to handleclick

--- src/components/Keys/Key.js
+++ src/components/Keys/Key.js
@@ -4,25 +4,25 @@ import Tone from 'tone';
 import './key.scss';
 
 function Key({ type }) {
-  const synth = new Tone.Synth({
-    oscillator: {
-      partials: [3, 2, 1],
-      type: 'custom',
-      frequency: 'C#4',
-      volume: -12,
-    },
-    envelope: {
-      attack: 0.11,
-      decay: 0.21,
-      sustain: 1,
-      release: 1.71,
-      attackCurve: 'exponential',
-      decayCurve: 'exponential',
-      releaseCurve: 'exponential',
-    },
-  }).toMaster();
-
   const handleClick = (key) => {
+    const synth = new Tone.Synth({
+      oscillator: {
+        partials: [3, 2, 1],
+        type: 'custom',
+        frequency: 'C#4',
+        volume: -12,
+      },
+      envelope: {
+        attack: 0.11,
+        decay: 0.21,
+        sustain: 1,
+        release: 1.71,
+        attackCurve: 'exponential',
+        decayCurve: 'exponential',
+        releaseCurve: 'exponential',
+      },
+    }).toMaster();
+
     synth.triggerAttackRelease(key, '8n');
   };
 
